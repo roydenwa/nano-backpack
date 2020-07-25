670c1da171a6fe3142c9d33f7ebe53dbe0107c99
roydenwa <royden-wagner@outlook.com>
2020-07-25 20:11:28 +0200

    added func to stream webcam

--- code/depth_estimation/DenseDepth/monocular.py
+++ code/depth_estimation/DenseDepth/monocular.py
@@ -3,6 +3,15 @@ import cv2
 import tensorflow as tf
 
 
-print(PIL.__version__)
-print(cv2.__version__)
-print(tf.__version__)
+def stream_webcam(cap):
+    while(True):
+        ret, frame = cap.read()
+        cv2.imshow("frame", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(cv2.CAP_DSHOW)
+    stream_webcam(cap)
