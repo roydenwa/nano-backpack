3d78f08cf00592da15f2789b40191630c30c7719
roydenwa <royden-wagner@outlook.com>
2020-07-25 21:20:17 +0200

    added dataclass for video input

--- code/depth_estimation/DenseDepth/monocular.py
+++ code/depth_estimation/DenseDepth/monocular.py
@@ -2,8 +2,11 @@ import PIL
 import cv2
 import tensorflow as tf
 
+from dataclasses import dataclass, field
+
 
 def stream_webcam(cap):
+    ''' end video-stream by pressing Q '''
     while(True):
         ret, frame = cap.read()
         cv2.imshow("frame", frame)
@@ -12,6 +15,31 @@ def stream_webcam(cap):
             break
 
 
+@dataclass
+class Video:
+    '''
+    Dataclass to manage video input with cv2
+
+    src = 0 -> webcam
+    '''
+    src: int
+    cap: object = field(init=False)
+
+    def __post_init__(self):
+        self.cap = cv2.VideoCapture(self.src)
+
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        if ret:
+            return frame
+
+
 if __name__ == "__main__":
-    cap = cv2.VideoCapture(cv2.CAP_DSHOW)
-    stream_webcam(cap)
+    video = Video(cv2.CAP_DSHOW)
+
+    while(True):
+        frame = video.get_frame()
+        cv2.imshow("frame", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
