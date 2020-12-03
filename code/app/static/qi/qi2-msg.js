ecd59674bd6fc3a7fa0aee1c86eaca4ba49a0091
roydenwa <royden-wagner@outlook.com>
2020-12-03 21:24:05 +0100

    use QiSession object in every func

--- code/app/static/qi/qi2-msg.js
+++ code/app/static/qi/qi2-msg.js
@@ -2,9 +2,39 @@ var testMsg = "imported msg";
 var session;
 
 function printMsg() {
-    console.log(testMsg + "from external func");
+    console.log(testMsg + " from external func");
 }
 
+function speak(msg) {
+  QiSession(function(session) {
+    console.log("connected!");
+
+    session.service("ALTextToSpeech").then(function(tts) {
+      tts.say(msg);
+    }, function(error) {
+      console.log(error);
+    });
+  }, function() {
+    console.log("disconnected");
+  });
+}
+
+function walk(x, y, theta) {
+  QiSession(function(session) {
+    console.log("connected!");
+
+    session.service("ALMotion").then(function(motion) {
+      // (x, y, theta) [m, m, rad] theta pos -> left, theta neg -> right
+      motion.moveTo(x, y, theta);
+    }, function(error) {
+      console.log(error);
+    });
+  }, function() {
+    console.log("disconnected");
+  });
+}
+
+// experimental to share session over global var
 function initQiSession(robotIp) {
     try {
         QiSession(function(s) {
@@ -14,21 +44,4 @@ function initQiSession(robotIp) {
     } catch (error) {
         console.log(error);
     }
-}
-
-function speak(msg) {
-    session.service("ALTextToSpeech").then(function(tts) {
-        tts.say(msg);
-    }, function(error) {
-        console.log(error);
-    })
-}
-
-function walk(x, y, theta) {
-    session.service("ALMotion").then(function(motion) {       
-        // (x, y, theta) [m, m, rad] theta pos -> left, theta neg -> right 
-        motion.moveTo(x, y, theta);
-    }, function(error) {
-        console.log(error);
-    })
 }
