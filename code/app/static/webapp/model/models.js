222c0fd868ee8fe0d802ee9668427eec1a286485
roydenwa <royden-wagner@outlook.com>
2020-12-03 21:26:18 +0100

    removed unused oData

--- code/app/static/webapp/model/models.js
+++ code/app/static/webapp/model/models.js
@@ -4,16 +4,8 @@ sap.ui.define([
     return {
       createModel: function() {
         var oData = {
-          features: [
-            "Enterprise-Ready Web Toolkit",
-            "Powerful Development Concepts",
-            "Feature-Rich UI Controls",
-            "Consistent User Experience",
-            "Free and Open Source",
-            "Responsive Across Browsers and Devices"
-          ]
+          robotIp = ""
         }
-
         var oModel = new sap.ui.model.json.JSONModel(oData);
 
         return oModel;
